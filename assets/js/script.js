b2d26444538e4340d646007c1bcf5026a080b130
moisesBarsoti <moises.barsot2@gmail.com>
2024-07-06 00:02:15 -0300

    Subindo arquivos

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+function instalarAplicativo(espacoDisponivel, conexaoInternet) {
+    // Verificar se há espaço disponível no dispositivo
+    if (espacoDisponivel >= tamanhoDoAplicativo) {
+        // Verificar a conexão com a internet
+        if (conexaoInternet) {
+            console.log("Baixar e instalar o aplicativo");
+            console.log("O aplicativo foi instalado com sucesso!");
+        } else {
+            return "Conectar-se à internet para instalar o aplicativo";
+        }
+    } else {
+        return "Liberar espaço no dispositivo para instalar o aplicativo";
+    }
+}
+
+// Tamanho aproximado do aplicativo em MB
+const tamanhoDoAplicativo = 50;
+
+// Scroll
+
+// Selecionando Elementos
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('nav#links a');
+
+// Acionando Elementos
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let heigth = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + heigth) {
+            navLinks.forEach(links => {
+                links.classList.remove('ativo');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('ativo');
+            })
+        };
+    })
+}
+
+// Scroll view
+
+// Selecionando elementos
+
+const sectionsHidden = document.querySelectorAll('section.hidden');
+
+const observarSection = new IntersectionObserver( (entrada) => {
+    entrada.forEach((entry) => {
+        if(entry.isIntersecting === true) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+});
+
+sectionsHidden.forEach((elementos) => observarSection.observe(elementos));
